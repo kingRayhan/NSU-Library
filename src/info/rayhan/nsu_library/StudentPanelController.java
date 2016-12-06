c75ad5d8c9251692e4f25a4f790b552a12dd33c1
kingRayhan <rayhan095@gmail.com>
2016-12-06 19:29:09 +0600

    create new student panel view and student panel controller

--- src/info/rayhan/nsu_library/StudentPanelController.java
+++ src/info/rayhan/nsu_library/StudentPanelController.java
@@ -5,9 +5,17 @@
  */
 package info.rayhan.nsu_library;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,12 +24,37 @@ import javafx.fxml.Initializable;
  */
 public class StudentPanelController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
+
+    @FXML
+    private void searchBooks(ActionEvent event) throws IOException {
+        Parent x=FXMLLoader.load(getClass().getResource("/views/SearchBook.fxml")); 
+        Scene  M= new Scene(x);
+        Stage app=(Stage)((Node)event.getSource()).getScene().getWindow();
+        app.setScene (M);
+        app.show();
+    }
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+        Parent x=FXMLLoader.load(getClass().getResource("/views/welcome.fxml")); 
+        Scene  M= new Scene(x);
+        Stage app=(Stage)((Node)event.getSource()).getScene().getWindow();
+        app.setScene (M);
+        app.show();
+    }
+
+    @FXML
+    private void BookList(ActionEvent event) throws IOException {
+        Parent x=FXMLLoader.load(getClass().getResource("/views/BookList.fxml")); 
+        Scene  M= new Scene(x);
+        Stage app=(Stage)((Node)event.getSource()).getScene().getWindow();
+        app.setScene (M);
+        app.show();
+    }
     
 }
